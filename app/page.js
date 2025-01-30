3edcd379559e9ce8171a70e47bd4149e85640852
GitCameronish <kevin@talent-connect.net>
2025-01-30 15:22:35 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import WorkoutTracker from '@/components/WorkoutTracker';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <WorkoutTracker />
+    </main>
+  );
+}
