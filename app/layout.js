e6d72b8cceb9276b5ed425df28e0a3896446e09a
GitCameronish <kevin@talent-connect.net>
2025-01-31 11:51:20 -0500

    Commit directly to the main branch

--- app/layout.js
+++ app/layout.js
@@ -6,10 +6,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-      </head>
       <body>{children}</body>
     </html>
   )
